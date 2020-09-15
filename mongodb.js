a28327e7a31e64fd1f3347b3f7cdb14171ca45b1
Thai Son Vu <vuthaison198x@gmail.com>
2020-09-15 14:44:05 +0700

    The ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,9 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const {MongoClient, ObjectID} = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
     if (error) {
@@ -12,15 +13,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     //CREATE DATABASE
     const db = client.db(databaseName)
 
-    // db.collection('user').insertOne({
-    //     name: 'Tommy',
-    //     age: 27
-    // },(error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert User')
-    //     }
-    //     console.log(result.ops)
-    // })
+    db.collection('user').insertOne({
+        _id: id,
+        name: 'Robert Binh',
+        age: 25
+    },(error, result) => {
+        if (error) {
+            return console.log('Unable to insert User')
+        }
+        console.log(result.ops)
+    })
 
     // db.collection('user').insertMany([
     //     {
@@ -39,24 +41,24 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     //     console.log(result.ops)
     // })
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Dev',
-            completed: false
-        },
-        {
-            description: 'Learn something new',
-            completed: true
-        },
-        {
-            description: 'Doing excercise',
-            completed: false
-        }
-    ],(error, result) => {
-        if (error) {
-            return console.log('Unable to insert new data')
-        }
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Dev',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'Learn something new',
+    //         completed: true
+    //     },
+    //     {
+    //         description: 'Doing excercise',
+    //         completed: false
+    //     }
+    // ],(error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert new data')
+    //     }
 
-        console.log(result.ops)
-    })
+    //     console.log(result.ops)
+    // })
 })
