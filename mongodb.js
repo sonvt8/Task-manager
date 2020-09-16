902596e178d0c544a1d1b3a4edb438acc728f0a0
Thai Son Vu <vuthaison198x@gmail.com>
2020-09-16 10:50:46 +0700

    Querry by ID

--- mongodb.js
+++ mongodb.js
@@ -3,7 +3,6 @@ const {MongoClient, ObjectID} = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
     if (error) {
@@ -13,52 +12,11 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     //CREATE DATABASE
     const db = client.db(databaseName)
 
-    db.collection('user').insertOne({
-        _id: id,
-        name: 'Robert Binh',
-        age: 25
-    },(error, result) => {
+    db.collection('users').findOne({_id : new ObjectID("5f606fa462528024fe2422c5")}, (error, user) => {
         if (error) {
-            return console.log('Unable to insert User')
+            return console.log('Unable to fetch')
         }
-        console.log(result.ops)
+
+        console.log(user)
     })
-
-    // db.collection('user').insertMany([
-    //     {
-    //         name: 'Nhung Bach',
-    //         age: 28
-    //     },
-    //     {
-    //         name: 'Son Vu',
-    //         age: 32
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert documents')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Dev',
-    //         completed: false
-    //     },
-    //     {
-    //         description: 'Learn something new',
-    //         completed: true
-    //     },
-    //     {
-    //         description: 'Doing excercise',
-    //         completed: false
-    //     }
-    // ],(error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert new data')
-    //     }
-
-    //     console.log(result.ops)
-    // })
 })
