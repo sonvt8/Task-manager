b1b83cffc038fd92b9ec04c05d224ce2d25e9f77
Thai Son Vu <vuthaison198x@gmail.com>
2020-09-17 21:00:25 +0700

    Create model 'Task'

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,22 +6,38 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String
+//     },
+//     age: {
+//         type: Number
+//     }
+// })
+
+// const me = new User({
+//     name: 'Tommy',
+//     age: 32
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((err) => {
+//     console.log('Error!',err)
+// })
+
+const Task = mongoose.model('Task',{
+    description: {type: String},
+    completed: {type: Boolean}
 })
 
-const me = new User({
-    name: 'Tommy',
-    age: 32
+const myTask = new Task({
+    description: 'Training two hours in Dev',
+    completed: false
 })
 
-me.save().then(() => {
-    console.log(me)
+myTask.save().then(() => {
+    console.log(myTask)
 }).catch((err) => {
-    console.log('Error!',err)
+    console.log(err)
 })
