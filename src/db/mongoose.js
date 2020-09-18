9a0063c7ddbc4ca03f805eb4951d9b3e4965d4a7
Thai Son Vu <vuthaison198x@gmail.com>
2020-09-18 11:44:03 +0700

    Set up first API and test with Postman

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,63 +1,10 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 })
-
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         lowercase: true,
-//         validate(value){
-//             if (!validator.isEmail(value)) {
-//                 throw new Error('Email is invalid')
-//             }
-//         }
-//     },
-//     age: {
-//         type: Number,
-//         default: 0,
-//         validate(value){
-//             if (value < 0) {
-//                 throw new Error('Age must be a positive number')
-//             }
-//         }
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//         minlength: 7,
-//         trim: true,
-//         validate(value){
-//             if (value.toLowerCase().includes('password')) {
-//                 throw new Error('Password cannot contain "password"')
-//             }
-//         }
-//     }
-// })
-
-// const me = new User({
-//     name: '   Vu    ',
-//     email: 'Tommyvu@gmail.com',
-//     age: 33,
-//     password: '123456p'
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((err) => {
-//     console.log('Error!',err.errors)
-// })
 
 const Task = mongoose.model('Task',{
     description: {
@@ -69,17 +16,4 @@ const Task = mongoose.model('Task',{
         type: Boolean,
         default: false,
     }
-})
-
-const myTask = new Task(
-    {
-        description: 'Doing excercise',
-        completed: true
-    },
-)
-
-myTask.save().then(() => {
-    console.log(myTask)
-}).catch((err) => {
-    console.log(err)
 })
