841a68df9fe989fb1231751b80419a1c1facce92
Thai Son Vu <vuthaison198x@gmail.com>
2020-10-21 16:18:28 +0700

    middleware does job before saving

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs');
 
-const Task = mongoose.model('Task',{
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -11,5 +12,7 @@ const Task = mongoose.model('Task',{
         default: false
     }
 })
+
+const Task = mongoose.model('Task',taskSchema)
 
 module.exports = Task
