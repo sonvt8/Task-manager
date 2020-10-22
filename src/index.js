2c520796cf6bbdbd21c5dbe5b1733a5e725da160
Thai Son Vu <vuthaison198x@gmail.com>
2020-10-22 10:53:16 +0700

    JWT

--- src/index.js
+++ src/index.js
@@ -13,3 +13,15 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const jwt = require('jsonwebtoken');
+
+const myFunction = async () => {
+    const token = jwt.sign({ _id: 'abc123'}, 'thisismylesson', {expiresIn : '7 days'})
+    console.log(token)
+
+    const data = jwt.verify(token, 'thisismylesson')
+    console.log(data)
+}
+
+myFunction()
