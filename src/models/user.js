b69a6d099973e5fccff77e8e5604ab8bffb0f0d8
Thai Son Vu <vuthaison198x@gmail.com>
2020-10-22 11:35:34 +0700

    Generate Authentication Token after creating user or login

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,7 +40,13 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Password cannot contain "password"')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
 userSchema.statics.findByCredentials = async (email, password) => {
@@ -55,6 +62,16 @@ userSchema.statics.findByCredentials = async (email, password) => {
     }
 
     return user
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({_id: user._id.toString()}, 'thisisMyClass')
+
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+
+    return token;
 }
 
 //Hash the plain text password before saving
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,26 +2,31 @@ const express = require('express');
 const User = require('../models/user')
 const router = new express.Router()
 
+//Create user
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     } catch (err) {
         res.status(400).send(err)
     }
 })
 
+//User login
 router.post('/users/login', async (req, res) => {
     try {
         const user =  await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({user, token})
     } catch (err) {
         res.status(400).send()
     }
 })
 
+//Read all users
 router.get('/users',async (req, res) => {
     try {
         const users = await User.find({})
@@ -31,6 +36,7 @@ router.get('/users',async (req, res) => {
     }
 })
 
+//Read user by Id
 router.get('/users/:id', async (req, res) => {
     const _id = req.params.id
 
@@ -42,6 +48,7 @@ router.get('/users/:id', async (req, res) => {
     }
 })
 
+//Update user by Id
 router.patch('/users/:id', async (req, res) => {
     const _id = req.params.id
     const updates = Object.keys(req.body)
@@ -67,6 +74,7 @@ router.patch('/users/:id', async (req, res) => {
     }
 })
 
+//Delete user by Id
 router.delete('/users/:id', async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id)
