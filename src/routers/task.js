5f0d0b55d2c9e8b719f5b5164ad395f785209d28
Thai Son Vu <vuthaison198x@gmail.com>
2020-10-29 18:51:44 +0700

    Filtering Data

--- src/routers/task.js
+++ src/routers/task.js
@@ -27,8 +27,16 @@ router.get('/tasks',async (req, res) => {
 })
 
 router.get('/tasks/my-tasks', auth, async (req, res) => {
+    const match = {}
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+    
     try {
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
         res.status(404).send(e)
