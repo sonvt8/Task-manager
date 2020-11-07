46f673f37fc5c983ff7a4f62f21e2dcc1e8cb705
Thai Son Vu <vuthaison198x@gmail.com>
2020-11-07 10:10:01 +0700

    Remove unuse data

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,7 +1,6 @@
-const sendgridAPIKey = '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'
 const sgMail = require('@sendgrid/mail')
 
-sgMail.setApiKey(sendgridAPIKey)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const  sendWelcomeEmail = (email, name) => {
     sgMail.send({
